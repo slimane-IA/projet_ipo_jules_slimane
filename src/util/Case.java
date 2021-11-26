f86f80b101fa798ea59c4577e13aefae13c85513
Julio974-Theexahedron <jules.bour.1@gmail.com>
2021-11-26 23:00:31 +0100

    Slight changes to Case

--- src/util/Case.java
+++ src/util/Case.java
@@ -3,24 +3,15 @@ package util;
 public class Case {
 	public final int absc;
 	public final int ord;
-	// public Case[] allCase;
-    // public boolean empty;
-	// public boolean isLastLine;/* if the case belongs to the last line ( the winning line)*/ 
 	
 	public Case(int absc, int ord) {
 		super();
 		this.absc = absc;
 		this.ord = ord;
-		// this.empty =true;
-		// this.isLastLine=false;
 	}
 
-
-
-	
 	@Override
 	public String toString(){
-		return "("+this.absc+","+this.ord+")";
+		return "("+this.absc+", "+this.ord+")";
 	}
-
 }
